46ff31e14156a9023fea4d596485168e2433833f
Rinehard <f1337s@outlook.de>
2024-07-09 21:26:34 +0200

    myout angepasst

--- mycsharp/myout/Program.cs
+++ mycsharp/myout/Program.cs
@@ -4,13 +4,27 @@ Console.WriteLine("Hello, Worldfff!");
 
 String myout(String input, out int lengthString)
 {
-    lengthString = input.lenght();
+    lengthString = input.Length;
     return input;
 }
 
-int lengthString;
 string myinputstring = "deine mudda";
-string outputString = myout(myinputstring);
+int lengthString;
+
+//Beim Aufrufen der Funktion mit dem Parameter "out" muss diese (ohne Deklaration int)
+//mit aufgeführt werden
+string outputString = myout(myinputstring, out lengthString);
 
 Console.WriteLine(outputString + " " + lengthString);
 
+int BerechneQuadrat(int zahl, out int quadrierteZahl)
+{
+    quadrierteZahl = zahl * zahl;
+    return zahl; // Funktion gibt den ursprünglichen Wert 'zahl' zurück
+}
+
+int zahl = 5;
+int quadrierteZahl;
+
+int ergebnis = BerechneQuadrat(zahl, out quadrierteZahl);
+Console.WriteLine($"Ergebnis: {ergebnis}, Quadrat: {quadrierteZahl}");
