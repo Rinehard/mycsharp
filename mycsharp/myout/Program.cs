378986805704abd3926971b7dc27efd5811c12ab
Rinehard <f1337s@outlook.de>
2024-07-09 21:18:25 +0200

    myout hinzugefügt

---
+++ mycsharp/myout/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, Worldfff!");
+
+
+String myout(String input, out int lengthString)
+{
+    lengthString = input.lenght();
+    return input;
+}
+
+int lengthString;
+string myinputstring = "deine mudda";
+string outputString = myout(myinputstring);
+
+Console.WriteLine(outputString + " " + lengthString);
+
